1bea02b23613f72fbb242a6f8c826d1ae97e6d7d
Chifer123536 <dun24821@gmail.com>
2025-03-30 07:34:15 +0300

    Гугл рекапчка

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,11 +1,20 @@
-// auth.module.ts
-import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { AuthController } from './auth.controller';
-import { UserModule } from '@/user/user.module'; 
+import { Module } from "@nestjs/common";
+import { AuthService } from "./auth.service";
+import { AuthController } from "./auth.controller";
+import { UserModule } from "@/user/user.module";
+import { GoogleRecaptchaModule } from "@nestlab/google-recaptcha";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { getRecaptchaConfig } from "@/config/recaptcha.config";
 
 @Module({
-  imports: [UserModule], 
+  imports: [
+    UserModule,
+    GoogleRecaptchaModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: getRecaptchaConfig,
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
