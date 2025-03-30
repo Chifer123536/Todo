2c1de0f45e874223cefb35b2ff74b7274d64b6ea
Chifer123536 <dun24821@gmail.com>
2025-03-30 07:14:34 +0300

    Гварды авторизации готовы, теперь только авторизованные пользователи могут отправлять гет запрос

---
+++ backend/src/auth/decorators/authorized.decorator.ts
@@ -0,0 +1,11 @@
+import { User } from "@/schemas/user.schema";
+import { createParamDecorator, ExecutionContext } from "@nestjs/common";
+
+export const AuthoRized = createParamDecorator(
+  (data: keyof User, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest();
+    const user = request.user;
+
+    return data ? user[data] : user;
+  }
+);
--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,7 +1,16 @@
-import { Controller } from '@nestjs/common';
-import { UserService } from './user.service';
+import { Controller, Get, HttpCode, HttpStatus } from "@nestjs/common";
+import { UserService } from "./user.service";
+import { AuthoRized } from "@/auth/decorators/authorized.decorator";
+import { Authorization } from "@/auth/decorators/auth.decorator";
 
-@Controller('users')
+@Controller("users")
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Authorization()
+  @HttpCode(HttpStatus.OK)
+  @Get("profile")
+  public async findFrofile(@AuthoRized("id") userId: string) {
+    return this.userService.findById(userId);
+  }
 }
