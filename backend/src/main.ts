00f1b97085b26732d3626c71c79a0ab5c9f820a9
Chifer123536 <dun24821@gmail.com>
2025-06-03 06:17:43 +0300

    Добавил env для продакшена, убрал лишнюю страку в main.ts бека, поправил зависимости

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,53 +1,52 @@
-import { ValidationPipe } from "@nestjs/common";
-import { ConfigService } from "@nestjs/config";
-import { NestFactory } from "@nestjs/core";
-import cookieParser from "cookie-parser";
-import session from "express-session";
-import { RedisStore } from "connect-redis";
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { NestFactory } from '@nestjs/core';
+import cookieParser from 'cookie-parser';
+import session from 'express-session';
+import { RedisStore } from 'connect-redis';
 
-import { AppModule } from "./app.module";
-import { ms, StringValue } from "./libs/common/utils/ms.util";
-import { parseBoolean } from "./libs/common/utils/parse-boolean.util";
-import { redisClient } from "./shared/redis/redis.client";
+import { AppModule } from './app.module';
+import { ms, StringValue } from './libs/common/utils/ms.util';
+import { parseBoolean } from './libs/common/utils/parse-boolean.util';
+import { redisClient } from './shared/redis/redis.client';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = app.get(ConfigService);
 
-  app.use(cookieParser(config.getOrThrow<string>("COOKIES_SECRET")));
+  app.use(cookieParser(config.getOrThrow<string>('COOKIES_SECRET')));
 
   app.useGlobalPipes(
     new ValidationPipe({
-      transform: true,
+      transform: true
     })
   );
 
   app.use(
     session({
-      secret: config.getOrThrow<string>("SESSION_SECRET"),
-      name: config.getOrThrow<string>("SESSION_NAME"),
+      secret: config.getOrThrow<string>('SESSION_SECRET'),
+      name: config.getOrThrow<string>('SESSION_NAME'),
       resave: true,
       saveUninitialized: false,
       cookie: {
-        domain: config.getOrThrow<string>("SESSION_DOMAIN"),
-        maxAge: ms(config.getOrThrow<StringValue>("SESSION_MAX_AGE")),
-        httpOnly: parseBoolean(config.getOrThrow<string>("SESSION_HTTP_ONLY")),
-        secure: parseBoolean(config.getOrThrow<string>("SESSION_SECURE")),
-        sameSite: "lax",
+        domain: config.getOrThrow<string>('SESSION_DOMAIN'),
+        maxAge: ms(config.getOrThrow<StringValue>('SESSION_MAX_AGE')),
+        httpOnly: parseBoolean(config.getOrThrow<string>('SESSION_HTTP_ONLY')),
+        secure: parseBoolean(config.getOrThrow<string>('SESSION_SECURE')),
+        sameSite: 'lax'
       },
       store: new RedisStore({
         client: redisClient,
-        prefix: config.getOrThrow<string>("SESSION_FOLDER"),
-      }),
+        prefix: config.getOrThrow<string>('SESSION_FOLDER')
+      })
     })
   );
 
   app.enableCors({
-    origin: config.getOrThrow<string>("ALLOWED_ORIGIN"),
-    credentials: true,
-    exposedHeaders: ["set-cookie"],
+    origin: config.getOrThrow<string>('ALLOWED_ORIGIN'),
+    credentials: true
   });
 
-  await app.listen(config.getOrThrow<number>("APPLICATION_PORT"));
+  await app.listen(config.getOrThrow<number>('APPLICATION_PORT'));
 }
 bootstrap();
