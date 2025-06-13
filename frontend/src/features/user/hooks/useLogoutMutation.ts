575cf021c32bafe7673ac02b0e9acb30fb202bba
Chifer123536 <dun24821@gmail.com>
2025-06-13 03:20:03 +0300

    изменил хук логаута, ибо оставался на прежней странице при удаления сессии

--- frontend/src/features/user/hooks/useLogoutMutation.ts
+++ frontend/src/features/user/hooks/useLogoutMutation.ts
@@ -1,18 +1,19 @@
-import { useMutation } from "@tanstack/react-query"
+import { useMutation, useQueryClient } from "@tanstack/react-query"
 import { useRouter } from "next/navigation"
 import { toast } from "sonner"
 
 import { authService } from "@/features/auth/services"
-
 import { toastMessageHandler } from "@/shared/utils"
 
 export function useLogoutMutation() {
   const router = useRouter()
+  const queryClient = useQueryClient()
 
   const { mutate: logout, isPending: isLoadingLogout } = useMutation({
     mutationKey: ["logout"],
     mutationFn: () => authService.logout(),
     onSuccess() {
+      queryClient.removeQueries({ queryKey: ["profile"] })
       toast.success("Successfully logged out.")
       router.push("/auth/login")
     },
